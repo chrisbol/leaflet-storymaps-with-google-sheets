5c1c987545e06fa8bcd86fa4a1ed7053beaac211
BlChrs <iu@tipmanager.com>
2019-09-28 17:18:02 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/144yZz3c0rW2jWwlpaKdPqZMB9zqodsPwJ_yFj5lB118/';
